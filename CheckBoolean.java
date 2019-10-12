91a3c51604ad9b932a3e1e44833eb830e7e462e7
Anton Jesuthas Nickson Joram <46377224+iamnickson@users.noreply.github.com>
2019-10-12 11:25:07 +0530

    Add files via upload

--- CheckBoolean.java
+++ CheckBoolean.java
@@ -1,53 +1,27 @@
 import java.util.Scanner;
 public class CheckBoolean
 {
-	public static boolean similar(int A[], int B[])
+	public static boolean majority(boolean a, boolean b, boolean c)
 	{
-		boolean check=false;
-		if(A.length==B.length)
+		boolean satisfied;
+		switch (value)
 		{
-			check=true;
-			int count=0;
-			for(int i=0; i<A.length; i++)
-			{
-				if(A[i]==B[i])
-					count=count+1;
-			}
-			if(count!=A.length)
-				check=false;
+			case 1:  satisfied = a&b;
+				break;
+		    case 2:  satisfied = a&c;
+				break;
+		    case 3:  satisfied = b&c;
+				break;
+		    case 4:  satisfied = a&b&c;
+				break;
+		    default: satisfied = false;
+				break;
 		}
-		return check;
+        return satisfied;
 	}
-	public static void main(String args[])
-	{
+    public static void main(String[] args)
+    {
 		Scanner input=new Scanner(System.in);
-		boolean check=false;
-		System.out.println("Enter the number of elements in array A");
-		int na=input.nextInt();
-		int A[]=new int[na];
-		System.out.println("Enter the elements of array A");
-		for(int i=0; i<na; i++)
-		{
-			A[i]=input.nextInt();
-		}
-		System.out.println("Enter the number of elements in array B");
-		int nb=input.nextInt();
-		int B[]=new int[nb];
-		System.out.println("Enter the elements of array B");
-		for(int i=0; i<nb; i++)
-		{
-			B[i]=input.nextInt();
-		}
-		System.out.println("Array length and corresponding pairs of elements are equal");
-		boolean value=similar(A, B);
-		
-		if(value)
-		{
-			System.out.println("True");
-		}
-		else
-		{
-			System.out.println("False");
-		}
-	}
+
+    }
 }
